0f80976fcc3a362204da89f37759a617a88e4fee
ehaider72 <ehaider@pacbell.net>
2017-01-25 23:16:44 -0800

    Game Outcomes
    Game outcomes were added for reference when coding them later

--- pic10c_assig1/siete_y_medio.cpp
+++ pic10c_assig1/siete_y_medio.cpp
@@ -18,6 +18,23 @@ using namespace std;
 
 // Stub for main
 int main(){
+
+	/* GAME OUTCOMES:
+	1) The player comes closer to 7 1/2 than the dealer or the dealer busts but the player did not bust.
+	In this case the player wins the round and the player's money increases by the amount that was bet.
+
+	2)The dealer comes closer to 7 1/2 than the player, or the player busts.
+	In this case the player loses the round and the player's money decreases by the amount that was bet.
+
+	3) Both, the player and dealer bust.
+	In this case the player loses the round and the player's money decreases by the amount that was bet.
+
+	4)This is called house advantage. 
+	Both the player and the dealer have the same total and  they do not bust.
+In this case a tie is declared and no money is exchanged.
+*/
+
+
 	/* --STATEMENTS-- */
 	return 0;
 }
